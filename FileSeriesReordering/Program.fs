071d4d8a3e3a1eaab8315c2c49b530a89ae1851e
Isaac Aronson <iaronson@decisiontek.com>
2018-08-17 21:49:43 -0400

    Output found range

--- FileSeriesReordering/Program.fs
+++ FileSeriesReordering/Program.fs
@@ -3,8 +3,23 @@ module Program
 
 open System
 open System.IO
+open System.Text.RegularExpressions
 
 module Program =
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
+        
+    let matchFirstNumberInString =
+        function 
+        | Regex @".*\d.*" [result] ->
+            match Int32.TryParse(result) with 
+            | (true, int) -> Some(int)
+            | _ -> None
+        | _ ->
+            None
+        
     let readFilenamesInDirectory (directory : DirectoryInfo) pattern =
         match directory.Exists with
         | false -> Seq.empty
@@ -13,7 +28,11 @@ module Program =
     [<EntryPoint>]
     let main argv =
         let currentDirectory = new DirectoryInfo(Environment.CurrentDirectory)
-        readFilenamesInDirectory currentDirectory "*.*" 
-        |> Seq.iter (printfn "%s")
+        let min, max = 
+            readFilenamesInDirectory currentDirectory "*.*" 
+            |> Seq.choose matchFirstNumberInString
+            |> Seq.sort
+            |> fun x -> Seq.min x, Seq.max x
+        printfn "Found range %i to %i" min max
         printfn "All files enumerated"
         0
